9adf87411079306b97f303832fdaa78c497d2423
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-17 17:07:21 +0000

    save

--- src/Splash.js
+++ src/Splash.js
@@ -1,4 +1,10 @@
 export default function SplashPage () {
+    const el = document.getElementById('load');
+    setTimeout(() => {
+        el.style.visibility = 'visible';
+      
+        
+      }, 4000);
     return (
         <div className="container">
             <div className="row justify-content-center align-items-center text-white my-3 text-center">
@@ -8,8 +14,8 @@ export default function SplashPage () {
                 <h3 className="col-3">Contact Me</h3>
             </div>
             <div className="row justify-content-center align-items-center text-white vh-100 text-center">
-                <h1 className="col-12 typewriter">Hello my name is Elijah Wenndt and I am a Full-Stack developer.</h1>
-                <div className="col-2 btn btn-primary align-self-start p-3">See my latest project</div>
+                <h2 className="col-12 typewriter">My name is Elijah Wenndt and I am a Full-Stack developer.</h2>
+                <div className="col-2 btn btn-primary align-self-start p-3" id='load'>See my latest project</div>
             </div>
         </div>
     )
