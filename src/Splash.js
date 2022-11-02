56cbf5924e78c192db76183c9dfa190acc5fa44c
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-11-02 13:45:41 +0000

    added styling

--- src/Splash.js
+++ src/Splash.js
@@ -7,15 +7,15 @@ export default function SplashPage () {
       }, 4000);
     return (
         <div className="container">
-            <div className="row justify-content-center align-items-center text-white my-3 text-center">
-                <h3 className="col-3">Projects</h3>
-                <h3 className="col-3">About Me</h3>
-                <h3 className="col-3">Blog</h3>
-                <h3 className="col-3">Contact Me</h3>
+            <div className="row justify-content-center align-items-center text-white mt-2 text-center position-relative">
+                <h3 className="col-3 hover fw-bold">Projects</h3>
+                <h3 className="col-3 hover fw-bold">About Me</h3>
+                <h3 className="col-3 hover fw-bold">Blog</h3>
+                <h3 className="col-3 hover fw-bold">Contact Me</h3>
             </div>
-            <div className="row justify-content-center align-items-center text-white vh-100 text-center">
+            <div className="row justify-content-center align-items-center text-white text-center vh-100">
                 <h2 className="col-12 typewriter">My name is Elijah Wenndt and I am a Full-Stack developer.</h2>
-                <div className="col-2 btn btn-primary align-self-start p-3" id='load'>See my latest project</div>
+                <div className="col-2 btn btn-primary align-self-start fade-in delay">Contact Me!</div>
             </div>
         </div>
     )
